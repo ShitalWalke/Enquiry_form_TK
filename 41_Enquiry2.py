db2d705abfb5211c782a7b65c44730bde347ae9d
ShitalWalke <shitalswalke44@gmail.com>
2024-07-17 15:57:33 +0530

    Update 41_Enquiry2.py

--- 41_Enquiry2.py
+++ 41_Enquiry2.py
@@ -3,7 +3,6 @@ from tkinter import ttk, messagebox
 from PIL import Image, ImageTk  # pip install pillow
 import pymysql
 
-
 class Register:
     def __init__(self, root):
         self.root = root
@@ -20,67 +19,58 @@ class Register:
         frame1 = Frame(self.root, bg="white")
         frame1.place(x=480, y=100, width=700, height=500)
 
-        title = Label(frame1, text="ENQUIRY HERE ", font=("times new roman", 20, "bold"), bg="white", fg="green").place(
-            x=50, y=30)
+        title = Label(frame1, text="ENQUIRY HERE ", font=("times new roman", 20, "bold"), bg="white", fg="green").place(x=50, y=30)
 
         # First Name
-        f_name = Label(frame1, text="First Name: ", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(
-            x=50, y=100)
+        f_name = Label(frame1, text="First Name: ", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(x=50, y=100)
         self.txt_fname = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_fname.place(x=50, y=130, width=250)
 
         # Last Name
-        l_name = Label(frame1, text="Last Name: ", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(
-            x=365, y=100)
+        l_name = Label(frame1, text="Last Name: ", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(x=365, y=100)
         self.txt_lname = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_lname.place(x=370, y=130, width=250)
 
         # Contacts
-        f_contacts = Label(frame1, text="Contact No: ", font=("times new roman", 15, "bold"), bg="white",
-                           fg="gray").place(x=50, y=160)
+        f_contacts = Label(frame1, text="Contact No: ", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(x=50, y=160)
         self.txt_contacts = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_contacts.place(x=50, y=190, width=250)
 
         # Email
-        l_email = Label(frame1, text="Email", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(x=365,
-                                                                                                                 y=160)
+        l_email = Label(frame1, text="Email", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(x=365, y=160)
         self.txt_email = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_email.place(x=370, y=190, width=250)
 
         # Security Question
-        s_question = Label(frame1, text="Security Question: ", font=("times new roman", 15, "bold"), bg="white",
-                           fg="gray").place(x=50, y=225)
+        s_question = Label(frame1, text="Security Question: ", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(x=50, y=225)
         self.comb_question = ttk.Combobox(frame1, font=("times new roman", 12), state='readonly', justify=CENTER)
         self.comb_question['values'] = ("Select", "your first name", "Your Last Name", "Your Birth Date")
         self.comb_question.place(x=50, y=255, width=250)
         self.comb_question.current(0)
 
         # Answer
-        s_answer = Label(frame1, text="Answer", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(
-            x=365, y=225)
+        s_answer = Label(frame1, text="Answer", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(x=365, y=225)
         self.txt_answer = Entry(frame1, font=("times new roman", 15), bg="lightgray")
         self.txt_answer.place(x=370, y=255, width=250)
 
         # Password
         c_password = Label(frame1, text="Password", font=("times new roman", 15, "bold"), bg="white", fg="gray").place(
             x=50, y=300)
-        self.txt_psw = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_psw = Entry(frame1, font=("times new roman", 15), bg="lightgray", show="*")
         self.txt_psw.place(x=50, y=340, width=250)
 
         # Confirm Password
         con_password = Label(frame1, text="Confirm Password", font=("times new roman", 15, "bold"), bg="white",
                              fg="gray").place(x=365, y=300)
-        self.txt_cpsw = Entry(frame1, font=("times new roman", 15), bg="lightgray")
+        self.txt_cpsw = Entry(frame1, font=("times new roman", 15), bg="lightgray", show="*")
         self.txt_cpsw.place(x=370, y=340, width=250)
 
         # Terms and Conditions
         self.chk = IntVar()
-        chk = Checkbutton(frame1, text="I agree to the terms and conditions", variable=self.chk, onvalue=1, offvalue=0,
-                          bg="White", font=("times new roman", 12)).place(x=50, y=380)
+        chk = Checkbutton(frame1, text="I agree to the terms and conditions", variable=self.chk, onvalue=1, offvalue=0, bg="White", font=("times new roman", 12)).place(x=50, y=380)
 
         # Submit Button
-        self.btn_smb = Button(frame1, text="Submit", font=("times new roman", 15, "bold"), bg="green", fg="gray",
-                              command=self.register_data).place(x=50, y=420)
+        self.btn_smb = Button(frame1, text="Submit", font=("times new roman", 15, "bold"), bg="green", fg="gray", command=self.register_data).place(x=50, y=420)
 
         # Sign In Button
         b_signin = Button(self.root, text="Sign In", font=("times new roman", 20), bd=0).place(x=250, y=490)
